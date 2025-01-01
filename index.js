cdb028816d73874e6180257d94ff44b564783c35
mdashraful24 <mdashrafulislam2882@gmail.com>
2025-01-01 18:20:48 +0600

    added vercel.json file

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'https://marathonproject-2a4f2.web.app',
+        'https://marathonproject-2a4f2.firebaseapp.com'
+    ],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
+
+
+// const logger = (req, res, next) => {
+//     console.log('inside the logger');
+//     next();
+// }
+
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token;
+    // console.log('inside verify token middleware', req.cookies);
+
+    if (!token) {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'Unauthorized access' })
+        }
+        req.user = decoded;
+        next();
+    })
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p8flg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -20,23 +54,55 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.connect();
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
+        // Collections
         const marathonsCollection = client.db('marathonRace').collection('marathons');
         const registrationsCollection = client.db('marathonRace').collection('registrations');
 
+
+        // Auth related APIs
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production'
+                })
+                .send({ success: true });
+        });
+
+        app.post('/logout', (req, res) => {
+            res
+                .clearCookie('token', {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production'
+                })
+                .send({ success: true })
+        })
+
+
         app.get('/marathons', async (req, res) => {
+            // console.log('now inside the api callback');
             const cursor = marathonsCollection.find().limit(6);
             const result = await cursor.toArray();
             res.send(result);
         });
 
-        app.get('/marathonPage', async (req, res) => {
-            const cursor = marathonsCollection.find();
-            const result = await cursor.toArray();
-            res.send(result);
+        app.get('/marathonPage', verifyToken, async (req, res) => {
+            // console.log("hello", req.cookies)
+            try {
+                const sortOrder = req.query.sortOrder === 'asc' ? 1 : -1; // Determine sorting order
+                const cursor = marathonsCollection.find().sort({ createdAt: sortOrder }); // Sort by createdAt field
+                const result = await cursor.toArray(); // Fetch results as an array
+                res.send(result); // Send the sorted results back to the client
+            } catch (error) {
+                console.error('Error fetching marathons:', error);
+                res.status(500).send({ message: 'Failed to fetch marathons' }); // Handle errors
+            }
         });
 
         app.get('/marathons/:id', async (req, res) => {
@@ -46,9 +112,19 @@ async function run() {
             res.send(result);
         });
 
-        app.get('/myApplyList', async (req, res) => {
-            const cursor = registrationsCollection.find();
+        app.get('/myApplyList', verifyToken, async (req, res) => {
+            const title = req.query.title || '';
+            const cursor = registrationsCollection.find({
+                title: { $regex: new RegExp(title, 'i') } // Case-insensitive search
+            });
             const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        app.post('/addMarathons', async (req, res) => {
+            const newMarathon = req.body;
+            // console.log(newMarathon);
+            const result = await marathonsCollection.insertOne(newMarathon);
             res.send(result);
         })
 
@@ -84,22 +160,42 @@ async function run() {
             res.send(result);
         })
 
+        app.put('/marathonPage/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updatedList = req.body;
+            const campaign = {
+                $set: {
+                    title: updatedList.title,
+                    firstName: updatedList.firstName,
+                    lastName: updatedList.lastName,
+                    number: updatedList.number,
+                    startRegistrationDate: updatedList.startRegistrationDate,
+                    endRegistrationDate: updatedList.endRegistrationDate,
+                    marathonStartDate: updatedList.marathonStartDate,
+                    runningDistance: updatedList.runningDistance,
+                    description: updatedList.description,
+                    marathonImage: updatedList.marathonImage,
+                }
+            }
+            const result = await marathonsCollection.updateOne(filter, campaign, options);
+            res.send(result);
+        })
+
+        app.delete('/marathonPage/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await marathonsCollection.deleteOne(query);
+            res.send(result);
+        })
+
         app.delete('/myApplyList/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await registrationsCollection.deleteOne(query);
             res.send(result);
         });
-
-
-        // app.patch("/marathons/increment/:title", async (req, res) => {
-        //     const title = req.params.title;
-        //     const result = await marathonsCollection.updateOne(
-        //         { title },
-        //         { $inc: { totalRegistrationCount: 1 } }
-        //     );
-        //     res.send(result);
-        // });
 
     } finally {
         // Ensures that the client will close when you finish/error
@@ -113,5 +209,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Marathon running at: ${port}`);
+    // console.log(`Marathon running at: ${port}`);
 });
